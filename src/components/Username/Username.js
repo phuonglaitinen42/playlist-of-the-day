9fb13bfe4b5ecab01409a31c90b64adce7157200
otsolap <59653348+otsolap@users.noreply.github.com>
2020-05-26 15:01:11 +0300

    {username} username page atm breaks the entire code. Need debugging.

--- src/components/Username/Username.js
+++ src/components/Username/Username.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignature } from "@fortawesome/free-solid-svg-icons";
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-const Username = (props) => {
+
+const Username = () => {
+    const [registeredName, setUserName] = useState({
+        username: "",
+    });
+
+    const changeValueHandler = (u) => {
+        setUserName({
+            ...registeredName,
+            [u.target.name]: u.target.value,
+        });
+    };
+
+    const addPostHandler = (u) => {
+        u.preventDefault();
+
+        axios
+            .post("http:localhost:3001/usernames", registeredName)
+            .then(response => {
+                console.log(response.data);
+            })
+    };
+
     return (
-        <Form className="name">
+
+        <Form className="registeredName" onSubmit={addPostHandler}>
             <Form.Group>
                 <Form.Label htmlFor="name">Please provide your name.</Form.Label>
-                <Form.control id="name" name="name" type="text" placeholder="John Doe"> </Form.control>
+                <Form.control
+                    id="name"
+                    name="name"
+                    type="text"
+                    placeholder="John Doe"
+                    onChange={changeValueHandler}
+                >
+                </Form.control>
             </Form.Group>
-
             <Button className="d-inline-block" variant="success" type="submit">
                 Send
-                </Button>
+                <FontAwesomeIcon icon={faSignature} className="signature-icon" />
+            </Button>
         </Form>
     )
 }
