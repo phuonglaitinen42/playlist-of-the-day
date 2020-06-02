c9eb75c12a053cb5b71313dcd8332619a277ecd4
phamhavy171 <59789678+phamhavy171@users.noreply.github.com>
2020-06-02 15:55:26 +0300

    test a call

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,12 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+// import { faCloudMeatball } from "@fortawesome/free-solid-svg-icons";
+const spotifyApi = new SpotifyWebApi();
 
-const Playlist = () => {
-  return (
-    <div>
-      <input type="button" value="Get your playlist"></input>
-      <div className="playlist-result"></div>
-    </div>
-  );
-};
+class Playlist extends Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    console.log(params);
+    const token = params.access_token;
+    if (token) {
+      spotifyApi.setAccessToken(token);
+    }
+    this.state = {
+      loggedIn: token ? true : false,
+      playlistName: "",
+      link: null,
+    };
+  }
+  getHashParams() {
+    var hashParams = {};
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
 
+  getPlaylist() {
+    spotifyApi.searchPlaylists("43ZHCT0cAZBISjO8DG9PnE").then(
+      (response) => {
+        const data = response.playlists.items[0].name;
+        const openSpotify = response.playlists.items[0].external_urls.spotify;
+        console.log(openSpotify);
+        console.log(data);
+        console.log(response);
+        this.setState({
+          playlistName: data,
+          link: openSpotify,
+        });
+      }
+      // function (data) {
+      //   console.log(data.playlists.items[0].uri);
+      // },
+      // function (err) {
+      //   console.error(err);
+      // }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="button"
+          value="Get your playlist"
+          onClick={() => this.getPlaylist()}
+        ></input>
+        <div className="playlist-result">{this.state.playlistName}</div>
+        <div>{this.state.link}</div>
+      </div>
+    );
+  }
+}
 export default Playlist;
