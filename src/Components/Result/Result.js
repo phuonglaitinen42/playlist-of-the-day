56fb04f801deec049817fafeb9fee7a9bd97e72d
Phuong Ngo <ngo.phuong4296@gmail.com>
2020-06-02 10:58:46 +0300

    created share buttons on facebook and twitter

--- src/Components/Result/Result.js
+++ src/Components/Result/Result.js
@@ -2,14 +2,19 @@ import React from "react";
 
 import PropTypes from "prop-types";
 import { CSSTransitionGroup } from "react-transition-group";
-import Button from 'react-bootstrap/Button';
-
+import Button from "react-bootstrap/Button";
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  FacebookIcon,
+  TwitterIcon,
+} from "react-share";
 function Result(props) {
-
   function refreshPage() {
     window.location.reload(false);
   }
-
+  const shareUrl = "http://localhost:3000/quiz";
+  const title = "Playlist of the Day";
   return (
     <CSSTransitionGroup
       className="container result"
@@ -25,10 +30,26 @@ function Result(props) {
         <strong>{props.quizResult} </strong>music at the momment!
       </div>
       <div>
-        <Button className="d-inline-block" variant="success" type="submit">Get your playlist now</Button>
-        <Button className="d-inline-block" variant="light" type="submit" onClick={refreshPage}>
-            Redo the questions
+        <Button className="d-inline-block" variant="success" type="submit">
+          Get your playlist now
         </Button>
+        <Button
+          className="d-inline-block"
+          variant="light"
+          type="submit"
+          onClick={refreshPage}
+        >
+          Redo the questions
+        </Button>
+        <div className="share-btn">
+          <h5>Share your Playlist of the Day with your friends</h5>
+          <FacebookShareButton url={shareUrl} quote={title}>
+            <FacebookIcon size={40} round={true} />
+          </FacebookShareButton>
+          <TwitterShareButton url={shareUrl} quote={title}>
+            <TwitterIcon size={40} round={true} />
+          </TwitterShareButton>
+        </div>
       </div>
     </CSSTransitionGroup>
   );
