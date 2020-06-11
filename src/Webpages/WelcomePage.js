ca453da645b9933f9c9a87cdddaa34697dd27e9a
phamhavy171 <59789678+phamhavy171@users.noreply.github.com>
2020-06-11 11:57:03 +0300

    take old photos

--- src/Webpages/WelcomePage.js
+++ src/Webpages/WelcomePage.js
@@ -6,7 +6,15 @@ const WelcomePage = () => {
   return (
     <div className="App-header">
       <h2>Find what playlist fits your feeling</h2>
-      <Username />
+      <p>
+        Playlist of the Day Application genereates a playlist which catches your
+        vibes in the day. We provide you a quick quiz to finalize music genre,
+        from there, newly created playlist is available for you after you have
+        logged in to Spotify. Now, let's start!
+      </p>
+      <a href="/quiz" class="btn btn-success">
+        Start quiz{" "}
+      </a>
     </div>
   );
 };
